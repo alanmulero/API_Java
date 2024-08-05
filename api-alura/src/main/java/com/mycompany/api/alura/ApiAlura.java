c633c60776138595fe726ff59a91bcd09a5af6af
alanmulero <alan.mulero@gmail.com>
2024-08-04 21:33:42 -0300

    Primeiro commit.

---
+++ api-alura/src/main/java/com/mycompany/api/alura/ApiAlura.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.api.alura;
+
+/**
+ *
+ * @author alan toledo
+ * data 4/08/24
+ * consumir um serviço de web services, usar uma API, testar, 
+ * trabalhar com as exceptions
+ */
+public class ApiAlura {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
