f25384a458f0cf14d84a791639c98213ccf52b50
alanmulero <alan.mulero@gmail.com>
2024-08-21 16:26:04 -0300

    Adicionando a classe PrincipalComBusca integrando uma API

---
+++ 2944-java-screenmatch-consumindo-webservice-main/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,34 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// Integrando com API
+		Scanner leitura = new Scanner(System.in);
+
+		// Implementando uma busca dinamica pelo teclado com a classe Scanner.
+		System.out.println("Digite um nome de filme para busca: ");
+		var busca = leitura.next();
+		var endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=6331e1f1";
+
+		// Chamando classe HttpRequest.
+		// Requisição.
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+
+		// Buscando a resposta da requisição pela Interface HttpResponse.
+
+		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+		System.out.println(response.body());
+
+	}
+
+}
